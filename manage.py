81d741e1aa36370cfc40f3ab7479a79a59280821
admin soporte <adminsoporte@cicat.lan>
2025-03-31 18:26:46 -0300

    Reestructuracion de la plataforma, tomando como directorio Raiz 'Proyecto_PGI-CS5'

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def main():
-    os.environ["DJANGO_SETTINGS_MODULE"] = "NewMembers.settings"
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewMembers.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
